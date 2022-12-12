d6f9ad07d5cef4a848aabd84c7f6bfe1fb01aef5
webformulator <mddalam1@gmail.com>
2022-12-12 22:47:56 +0530

    Removed unusual code and files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Leave Management System", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Leave Management System/i);
   expect(linkElement).toBeInTheDocument();
 });
