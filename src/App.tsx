b6aa349aa45b8c5fa0a3c86ff2eceaf779f489cf
webformulator <mddalam1@gmail.com>
2022-12-12 23:47:43 +0530

    Installed dependencies and initialized tailwind.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <h1>Leave Management System</h1>;
+  return (
+    <div className="text-center">
+      <h4 className="text-white bg-orange-400 text-3xl py-5">
+        Leave Management System
+      </h4>
+    </div>
+  );
 }
 
 export default App;
