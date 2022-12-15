01ba52c622beba3dd243d06ecd3b289e815ab568
webformulator <mddalam1@gmail.com>
2022-12-16 00:17:57 +0530

    Fix: Made header sticky and opaque.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { NoPropType } from "../types/component.types";
 
 const Header: NoPropType = () => {
   return (
-    <div className="text-center bg-orange-400">
+    <div className="sticky top-0 text-center bg-orange-400 opacity-95">
       <h4 className="text-white text-3xl py-5">Absence Management System</h4>
     </div>
   );
